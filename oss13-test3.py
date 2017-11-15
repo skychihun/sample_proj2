6973180a274a5580bbf2ba32952598cb7a81d250
ChiHeon Kwon <5chihun@naver.com>
2017-11-15 13:22:56 +0900

    my message

---
+++ oss13-test3.py
@@ -0,0 +1,20 @@
+te1 = {}
+te2 = {}
+te3 = {}
+
+te1['name'] = 'Lee'
+te1['num'] = 60131111
+te2['name'] = 'Seo'
+te2['num'] = 60160000
+te3['name'] = 'Kwon'
+te3['num'] = 60132228
+
+a= input()
+
+if(a in te1.values()):
+    print(te1)
+
+elif (a in te2.values()):
+    print (te2)
+elif (a in te3.values()):
+    print (te3)
